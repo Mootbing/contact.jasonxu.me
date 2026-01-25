41d31b6c5ccd753d113c29a800c91975755f97e2
Mootbing <mootbing@users.noreply.github.com>
2026-01-25 01:03:30 -0500

    shit

---
+++ app/components/jasonComponentContact/contactJason.constants.js
@@ -0,0 +1,26 @@
+// contactJason.constants.js
+// Shared constants for contactJason components
+
+export const MAX_DIMENSION = 1150;
+export const SMALL_ORBITAL_SCALE = 0.75;
+export const MEDIUM_ORBITAL_SCALE = 1;
+export const LARGE_ORBITAL_SCALE = 1.25;
+export const BALL_MIN_SIZE = 40;
+export const BALL_MAX_SIZE = 120;
+export const BALLS_PER_ROW = 5;
+export const CAROUSEL_OPACITY_LARGE = 0.25;
+export const CAROUSEL_OPACITY_MEDIUM = 0.35;
+export const CAROUSEL_OPACITY_SMALL = 0.15;
+export const TITLE_FONT_SIZE = 75;
+export const SUBTITLE_FONT_SIZE = 20;
+export const TITLE_MAX_WIDTH = 600;
+export const TITLE_MAX_WIDTH_SMALL = 250;
+export const TITLE_MARGIN_TOP_SMALL = 35;
+export const JASON_IMAGE_SIZE = 240;
+export const ANIMATION_BASE_DELAY = 0.5;
+export const ANIMATION_STEP = 0.1;
+export const ANIMATION_FADE_DELAY = 10;
+export const ANIMATION_FADE_DURATION = 900;
+export const SCREEN_WIDTH_THRESHOLD = 800;
+export const SCREEN_HEIGHT_THRESHOLD = 600;
+export const ROTATION_SPEED = 0.0005;
